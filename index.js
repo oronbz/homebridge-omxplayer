0a160953b7e40ef810c9beeaf751f0ae8c126ca3
Oron Ben Zvi <oronbz@gmail.com>
2017-08-09 00:29:29 +0300

    First try with hardcoded song

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var Service, Characteristic
+var exec = require("child_process").exec;
+var kill = require("tree-kill");
+var Service, Characteristic;
 
 module.exports = function(homebridge) {
     
-    Service = homebridge.hap.Service
-    Characteristic = homebridge.hap.Characteristic
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
     
-    homebridge.registerAccessory("homebridge-omxplayer", "OmxPlayer", OmxPlayer)
+    homebridge.registerAccessory("homebridge-omxplayer", "OmxPlayer", OmxPlayer);
 }
 
 function OmxPlayer(log, config) {
+    console.log("OmxPlayer plugin started!");
     this.log = log;
-    this.name = config.name
+    this.name = config.name;
+    this.omxpid = 0;
     
     this._service = new Service.Switch(this.name);
     this._service.getCharacteristic(Characteristic.On)
-        .on('set', this._setOn.bind(this))
+        .on('set', this._setOn.bind(this));
 }
 
 OmxPlayer.prototype.getServices = function() {
@@ -23,12 +27,14 @@ OmxPlayer.prototype.getServices = function() {
 
 OmxPlayer.prototype._setOn = function(on, callback) {
     
-    this.log("Setting omxplayer switch to " + on)
+    this.log("Setting omxplayer switch to " + on);
     
     if (on) {
-        this.log("playing the song")
+        this.log("playing the song");
+        this.omxpid = exec("omxplayer ~/song.mp4").pid;
     } else {
-        this.log("killing omx player")
+        this.log("killing omx player");
+        kill(this.omxpid);
     }
-    callback()
+    callback();
 }
